b8b35bda3662e0df15792c3c5e24ba5eb2c8bda6
Yura Kochubailo <yurii.kochubailo@chisw.com>
2023-11-29 11:31:38 +0100

    Update change password tests

--- src/test/java/golftests/ui/uitests/auth/ChangePassword.java
+++ src/test/java/golftests/ui/uitests/auth/ChangePassword.java
@@ -3,38 +3,90 @@ package golftests.ui.uitests.auth;
 import golf.ui.pages.HomePage;
 import golf.ui.pages.LoginPage;
 import golf.ui.pages.ProfilePage;
+import golf.utilities.EmailReceiver;
 import golftests.dataprovider.XlsDataProvider;
 import golftests.ui.uibase.BaseTest;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static golf.utilities.Constant.*;
 
 public class ChangePassword extends BaseTest {
-    @Test
-    public void userChangePassword() {
-        HomePage homePage = new HomePage(driver, wait);
-        LoginPage loginPage = new LoginPage(driver, wait);
-        homePage.openPage(BASE_URL_UI);
-        homePage.clickProfileButton();
-        loginPage.login(CLIENT_LOGIN, CLIENT_PASSWORD);
-    }
+    private String temporaryPassword;
+    private boolean runAfterMethod;
 
-    @Test(dataProvider = "InvalidChangePasswordData", dataProviderClass = XlsDataProvider.class)
-    public void userChangePasswordNegative(String currentPassword, String newPassword,
-                                           String confirmNewPassword, String expectedMessage) {
-        HomePage homePage = new HomePage(driver, wait);
-        LoginPage loginPage = new LoginPage(driver, wait);
+    private HomePage homePage;
+    private LoginPage loginPage;
+    private ProfilePage profilePage;
+
+    @BeforeMethod
+    public void setup() {
+        homePage = new HomePage(driver, wait);
+        loginPage = new LoginPage(driver, wait);
         homePage.openPage(BASE_URL_UI);
         homePage.clickProfileButton();
         loginPage.login(CLIENT_LOGIN, CLIENT_PASSWORD);
         homePage.isUserHomePageLoaded();
         homePage.clickProfileButton();
         loginPage.clickMyProfileBtn();
-        ProfilePage profilePage = new ProfilePage(driver, wait);
+        profilePage = new ProfilePage(driver, wait);
         profilePage.clickChangePasswordBtn();
+    }
+
+    @Test
+    public void userChangePassword() {
+        profilePage.changePassword(CLIENT_PASSWORD, CLIENT_NEW_PASSWORD, CLIENT_NEW_PASSWORD);
+        profilePage.clickSavePasswordBtn();
+        String html = EmailReceiver.getLastMailHtmlValue(CLIENT_LOGIN, CLIENT_MAIL_PASSWORD_831);
+        temporaryPassword = EmailReceiver.getTemporaryPasswordValue(html);
+        runAfterMethod = true;
+        homePage.clickProfileButton();
+        loginPage.login(CLIENT_LOGIN, CLIENT_NEW_PASSWORD);
+        Assert.assertTrue(homePage.isUserHomePageLoaded());
+    }
+    @Test
+    public void userChangePasswordAndLoginWithOldPasswordNegative() {
+        profilePage.changePassword(CLIENT_PASSWORD, CLIENT_NEW_PASSWORD, CLIENT_NEW_PASSWORD);
+        profilePage.clickSavePasswordBtn();
+        String html = EmailReceiver.getLastMailHtmlValue(CLIENT_LOGIN, CLIENT_MAIL_PASSWORD_831);
+        temporaryPassword = EmailReceiver.getTemporaryPasswordValue(html);
+        runAfterMethod = true;
+        homePage.clickProfileButton();
+        loginPage.login(CLIENT_LOGIN, CLIENT_PASSWORD);
+        loginPage.assertExactTextAppearedOnPage("Invalid email or password");
+    }
+
+    @Test(dataProvider = "InvalidChangePasswordData", dataProviderClass = XlsDataProvider.class)
+    public void userChangePasswordNegative(String currentPassword, String newPassword,
+                                           String confirmNewPassword, String expectedMessage) {
+        runAfterMethod = false;
         profilePage.changePassword(currentPassword, newPassword, confirmNewPassword);
         profilePage.assertExactTextAppearedOnPage(expectedMessage);
 
+    }
+
+    @AfterMethod
+    public void returnPreviousPassword() {
+        if (runAfterMethod) {
+            String requestBody = String.format("{\"email\":\"%s\",\"tempPassword\":\"%s\",\"newPassword\":\"%s\"}",
+                    CLIENT_LOGIN, temporaryPassword, CLIENT_PASSWORD);
+
+            RestAssured.given()
+                    .baseUri(BASE_URI)
+                    .basePath(PASSWORD_RECOVERY_BASE_PATH)
+                    .contentType(ContentType.JSON).given()
+                    .body(requestBody)
+                    .filter(new AllureRestAssured())
+                    .when()
+                    .post()
+                    .then()
+                    .statusCode(200);
+        }
     }
 
 }
